2368fb9401905fd59fa0263bfacaac1ad318bb56
fishforward <yy_8789@126.com>
2013-10-26 15:30:27 +0800

    tmp add

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,8 @@ Weixintui::Application.configure do
   # Do not compress assets
   config.assets.compress = false
 
+  config.assets.compile = true
+
   # Expands the lines which load the assets
   config.assets.debug = true
 
